40c2cf84a9cd1442ef8eaa7bd32c82013fc1c7a2
Chris Lawver <chris_lawver@yahoo.com>
2021-03-13 15:46:00 -0500

    completed autoclicker functionality, now checks if user has enough donuts for purchase and subtracts them

--- src/js/donutMaker.js
+++ src/js/donutMaker.js
@@ -4,7 +4,11 @@ let donutCountDisplay = document.getElementById("donut-count");
 let autoClickerCount = 0;
 let autoClickerDisplay = document.getElementById("auto-clicker-count");
 
-//Tracks amount of clicks and sets the donut count value
+let autoClickerCost = 100;
+let autoClickerCostDisplay = document.getElementById("buy-auto-clicker");
+autoClickerCostDisplay.innerText = `(${autoClickerCost} donuts)`;
+
+//Tracks amount of clicks and sets/displays the donut count value
 let donutClicker = document.getElementById("donut-clicker-button");
 donutClicker.addEventListener("click", addDonuts);
 
@@ -14,13 +18,27 @@ function addDonuts(){
     donutCountDisplay.innerText = `: ${donutCount}`;
 }
 
+//Subtracts amount of donuts based on the number that as passed in
+function subtractDonuts(number){
+    donutCount -= number;
+    console.log(`Donut count: ${donutCount}`);
+    donutCountDisplay.innerText = `: ${donutCount}`;
+}
+
 //Increments donut count per second based on how manny autoclickers are active
 let autoClicker = document.getElementById("auto-clicker-button");
 autoClicker.addEventListener("click", addAutoClicker);
 
 function addAutoClicker(){
-    setInterval(addDonuts, 1000);
-    autoClickerCount++;
-    console.log(`Autoclicker count: ${autoClickerCount}`);
-    autoClickerDisplay.innerText = `: ${autoClickerCount}`;
+    if(donutCount >= autoClickerCost){
+        setInterval(addDonuts, 1000);
+        autoClickerCount++;
+
+        subtractDonuts(Math.floor(autoClickerCost));
+        autoClickerCost += (autoClickerCost * .1);
+
+        console.log(`Autoclicker count: ${autoClickerCount}`);
+        autoClickerDisplay.innerText = `: ${autoClickerCount}`;
+        autoClickerCostDisplay.innerText = `(${Math.floor(autoClickerCost)} donuts)`;
+    }
 }
